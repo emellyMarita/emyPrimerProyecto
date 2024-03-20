66b691333b5132be200cff46e9c5d2bc896089e9
Glitch (glitch-hello-website) <none>
2024-03-20 02:57:26 +0000

    🐔🎀 Checkpoint ./script.js:66042137/2768

---
+++ script.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+  questionOne: 1. ¿Qué logro significativo alcanzó Nelly Carrillo Tarazona en el campo de la biología en la Universidad Nacional Mayor de San Marcos?,
+  answers: [
+  {text: "a) Descubrió una nueva especie de planta endémica de la selva peruana.", correct: true},
+  {text: " b) Publicó un libro sobre la biodiversidad marina del litoral peruano.", correct: false},
+  {text: " c) Fundó el primer laboratorio de biología molecular de la universidad", correct: false},
+  questionOne: 2.¿Cuál de las siguientes divulgadoras científicas peruanas se enfoca en el estudio de la ecología y la conservación de la biodiversidad?
+  answers: [
+  {text: "a) Descubrió una nueva especie de planta endémica de la selva peruana.", correct: true
+  text: " b) Publicó un libro sobre la biodiversidad marina del litoral peruano.", correct: false
+  text: " c) Fundó el primer laboratorio de biología molecular de la universidad", correct: false
+  
+}
+  
+  ]
+}]
