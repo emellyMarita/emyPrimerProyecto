5db14dad82d991e39991bb3bc62bce495c5d59ce
Glitch (glitch-hello-website) <none>
2024-03-20 03:09:29 +0000

    🍽🐴 Checkpoint ./script.js:66042137/1732 ./index.html:66042137/94

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const questions = [
   {
-  questionOne: 1. ¿Qué logro significativo alcanzó Nelly Carrillo Tarazona en el campo de la biología en la Universidad Nacional Mayor de San Marcos?,
+  questionOne: "1. ¿Qué logro significativo alcanzó Nelly Carrillo Tarazona en el campo de la biología en la Universidad Nacional Mayor de San Marcos?",
   answers: [
   {text: "a) Descubrió una nueva especie de planta endémica de la selva peruana.", correct: true},
   {text: " b) Publicó un libro sobre la biodiversidad marina del litoral peruano.", correct: false},
   {text: " c) Fundó el primer laboratorio de biología molecular de la universidad", correct: false},
-  questionOne: 2.¿Cuál de las siguientes divulgadoras científicas peruanas se enfoca en el estudio de la ecología y la conservación de la biodiversidad?
-  answers: [
-  {text: "a) Descubrió una nueva especie de planta endémica de la selva peruana.", correct: true
-  text: " b) Publicó un libro sobre la biodiversidad marina del litoral peruano.", correct: false
-  text: " c) Fundó el primer laboratorio de biología molecular de la universidad", correct: false
-  
+  ]
+},
+  {
+  questionTwo: "2.¿Cuál de las siguientes divulgadoras científicas peruanas se enfoca en el estudio de la ecología y la conservación de la biodiversidad?",
+    answers: [
+  {text: " Alejandra Ruiz León", correct: true},
+  {text: " Denisse Sotomayor", correct: false},
+  {text: " Natalia Sobrevilla", correct: false},
+  ]
 }
   
-  ]
-}]
+const questionElement = document.getElementById("questionOne");
