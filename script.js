1455b135a999657e90168954978744bfc2c9f91a
Glitch (glitch-hello-website) <none>
2024-03-20 04:43:37 +0000

    🌾🍤 Checkpoint ./index.html:66042137/497 ./script.js:66042137/365

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function result () {
   var score = 0;
   if(document.getElementById ("correct1").checked)
+  {
+    score ++;
+  }
+ if(document.getElementById ("correct2").checked)
+  {
+    score ++;
+  }
+  window.alert("Your score is "+score)
 }
